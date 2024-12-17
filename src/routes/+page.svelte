89721e84f90f0011305465d7fe28b16676bee53c
Joba Adewumi <jobaadewumis@gmail.com>
2024-12-17 11:27:57 +0100

    Tweaking bottom nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,35 @@
+<script lang="ts">
+</script>
+
 <section class="mx-7 mb-3 mt-5 md:mx-12">
 	<div>
-		<h2 class="font-pT my-3 bg-red-500 text-6xl md:text-8xl xl:text-9xl">This is the intro</h2>
+		<h2 class="font-pT my-3 bg-red-500 text-center text-6xl md:text-8xl xl:text-9xl">
+			This is the intro
+		</h2>
 	</div>
 	<div class="flex items-center justify-center">
-		<enhanced:img class=" w-1/2" src="$lib/assets/images/headshot.jpg" alt="An alt text" />
+		<div class="w-2/4">
+			<enhanced:img
+				class="mx-auto rounded-3xl"
+				src="$lib/assets/images/headshot.jpg"
+				alt="An alt text"
+			/>
+		</div>
 	</div>
-	<div><button type="button">CTA</button></div>
+	<div class="mb-5 mt-7 flex items-center justify-center">
+		<button class="h-16 w-24 rounded-2xl bg-blue-500 px-3 py-3" type="button">CTA</button>
+	</div>
 </section>
 
 <section>
-	<div>
-		<h3>About Me</h3>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque asperiores at labore eaque,
-			necessitatibus facilis temporibus facere voluptates amet nesciunt modi quis sed dolor
-			molestias vel quod assumenda magni laborum?
-		</p>
+	<div class="mx-3 flex justify-around">
+		<span> About Me </span>
+		<span> Portfolio </span>
+		<span> Services </span>
+		<span> Links </span>
 	</div>
+
 	<div>
-		<h3>Portfolio</h3>
-		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem saepe, eius eum sint quo rem
-			vero assumenda nemo minima dicta consectetur recusandae qui, praesentium dolore ratione
-			fugiat! Ipsa, sint perferendis.
-		</p>
-	</div>
-	<div>
-		<h3>Services</h3>
-		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus maxime consectetur, quis
-			atque earum, magni corrupti vitae facere iusto excepturi sunt. Mollitia vero omnis, in esse
-			quis minus dignissimos et.
-		</p>
-	</div>
-	<div>
-		<h3>Links</h3>
 		<p>
 			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum esse autem maiores. Eum
 			deserunt quaerat, quas nulla facere laborum, quasi iusto amet labore vero error repellendus
