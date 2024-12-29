a232d5e425548b7b0cf70c8999570edf4f87b353
Joba Adewumi <jobaadewumis@gmail.com>
2024-12-29 01:31:02 +0100

    Cleaned up about and skelenton for portfolio

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,17 +1,36 @@
-<p>About page</p>
+<script>
+	import Navbar from '$lib/components/navbar.svelte';
 
-<section>
+</script>
+
+<Navbar />
+
+<section class="my-10">
+	<h2 class="font-pT my-3 mx-3 bg-red-500 text-left text-3xl md:text-5xl xl:text-6xl">
+		ABOUT ME
+	</h2>
+</section>
+
+<section class="mx-7 mb-3 mt-5 md:mx-12">
 	<p>
 		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dicta labore nemo voluptates
 		voluptate perspiciatis provident distinctio itaque ipsam nihil voluptatum ut aperiam, aut
 		ducimus odit suscipit est delectus cupiditate?
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui ex, reiciendis esse tenetur rem et temporibus officiis! Totam nobis quae eum animi fugiat dolore iusto molestias earum iure, deserunt corrupti.
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur debitis at consectetur tempora ullam. Fugiat distinctio minima perferendis accusamus quia nam cumque, doloremque ad maxime recusandae culpa et placeat dignissimos.
 	</p>
 </section>
 
+<section class="my-10">
+	<hr class="mx-3 border-2 border-yellow-500" />
+	<h2 class="font-pT my-3 mx-3 bg-red-500 text-left text-3xl md:text-5xl xl:text-6xl">
+		SKILLS
+	</h2>
+</section>
+
 <section>
-	<h2>Skills</h2>
 	<div>
-		<h3>Technical</h3>
+		<h3 class="font-pT my-3 mx-3 bg-red-500 text-left text-1xl md:text-3xl xl:text-4xl">Technical</h3>
 		<p>
 			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate, delectus expedita?
 			Provident eveniet expedita nostrum ab, autem natus, aliquam maiores nam minima hic architecto
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,17 +1,117 @@
-<p>Portfolio page</p>
+<script>
+	import Navbar from '$lib/components/navbar.svelte';
+
+	const data = [
+		{
+			title: 'Video Editing & Videography',
+			image: 'src/lib/assets/images/project1.jpg',
+			alt: '',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium quod non, cumque repellat asperiores facilis porro unde voluptatibus maxime culpa exercitationem quo necessitatibus aut ullam fugiat earum, fuga delectus!',
+			link: '',
+			video: '',
+			podcastEmbed: '',
+			cta: 'CTA',
+			photo1: '',
+			photo2: '',
+			photo3: '',
+			photo4: '',
+			photo5: '',
+			photo6: '',
+		},
+		{
+			title: 'Writing & Editing',
+			image: 'src/lib/assets/images/project1.jpg',
+			alt: '',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium quod non, cumque repellat asperiores facilis porro unde voluptatibus maxime culpa exercitationem quo necessitatibus aut ullam fugiat earum, fuga delectus!',
+			link: '',
+			video: '',
+			podcastEmbed: '',
+			cta: 'CTA',
+			photo1: '',
+			photo2: '',
+			photo3: '',
+			photo4: '',
+			photo5: '',
+			photo6: '',
+		},
+		{
+			title: 'Podcast Editing & Production',
+			image: 'src/lib/assets/images/project1.jpg',
+			alt: '',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium quod non, cumque repellat asperiores facilis porro unde voluptatibus maxime culpa exercitationem quo necessitatibus aut ullam fugiat earum, fuga delectus!',
+			link: '',
+			video: '',
+			podcastEmbed: '',
+			cta: 'CTA',
+			photo1: '',
+			photo2: '',
+			photo3: '',
+			photo4: '',
+			photo5: '',
+			photo6: '',
+		},
+		{
+			title: 'Software Development & Web Development',
+			image: 'src/lib/assets/images/project1.jpg',
+			alt: '',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium quod non, cumque repellat asperiores facilis porro unde voluptatibus maxime culpa exercitationem quo necessitatibus aut ullam fugiat earum, fuga delectus!',
+			link: '',
+			video: '',
+			podcastEmbed: '',
+			cta: 'CTA',
+			photo1: '',
+			photo2: '',
+			photo3: '',
+			photo4: '',
+			photo5: '',
+			photo6: '',
+		},
+		{
+			title: 'Photography & Photo Editing',
+			image: 'src/lib/assets/images/project1.jpg',
+			alt: '',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium quod non, cumque repellat asperiores facilis porro unde voluptatibus maxime culpa exercitationem quo necessitatibus aut ullam fugiat earum, fuga delectus!',
+			link: '',
+			video: '',
+			podcastEmbed: '',
+			cta: 'CTA',
+			photo1: '',
+			photo2: '',
+			photo3: '',
+			photo4: '',
+			photo5: '',
+			photo6: '',
+		},
+	];
+
+
+</script>
+
+<Navbar />
+
+<section class="my-10">
+	<h2 class="font-pT my-3 mx-3 bg-red-500 text-left text-3xl md:text-5xl xl:text-6xl">
+		PORTFOLIO
+	</h2>
+</section>
 
 <section>
 	<!-- Would have to do a for each statement to render the multiple data -->
+	 {#each data as { title, image, alt, description, link, video, podcastEmbed, cta, photo1, photo2, photo3, photo4, photo5, photo6 }}
 	<div>
-		<h2></h2>
-		<div><img src="" alt="" /></div>
+		<h2>{title}</h2>
+		<div><img src="{image}" alt="{alt}" /></div>
 		<div>
 			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium quod non, cumque
-				repellat asperiores facilis porro unde voluptatibus maxime culpa exercitationem quo
-				necessitatibus aut ullam fugiat earum, fuga delectus!
+				{description}
 			</p>
 			<div><button type="button">CTA</button></div>
 		</div>
 	</div>
+	{/each}
 </section>
