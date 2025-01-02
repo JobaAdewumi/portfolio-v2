551f84d3bcbfc8d367ba79e4a8376af1378fccb8
Joba Adewumi <jobaadewumis@gmail.com>
2025-01-03 00:32:18 +0100

    added footer, styling and pictures

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,51 +1,18 @@
 <script>
-	import Navbar from '$lib/components/navbar.svelte';
+	import Footer from '$lib/components/footer.svelte';
+import Navbar from '$lib/components/navbar.svelte';
 
 	const data = [
 		{
 			title: 'Video Editing & Videography',
-			image: 'src/lib/assets/images/project1.jpg',
+			image: '',
 			alt: '',
 			description:
 				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium quod non, cumque repellat asperiores facilis porro unde voluptatibus maxime culpa exercitationem quo necessitatibus aut ullam fugiat earum, fuga delectus!',
-			link: '',
-			video: '',
+			link: 'https://www.youtube.com/channel/UCZGvrbbwfQw3YyKvfIt3t4A',
+			video: 'https://www.youtube.com/embed/uZ50Kbc2Jpg',
 			podcastEmbed: '',
-			cta: 'CTA',
-			photo1: '',
-			photo2: '',
-			photo3: '',
-			photo4: '',
-			photo5: '',
-			photo6: ''
-		},
-		{
-			title: 'Writing & Editing',
-			image: 'src/lib/assets/images/project1.jpg',
-			alt: '',
-			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium quod non, cumque repellat asperiores facilis porro unde voluptatibus maxime culpa exercitationem quo necessitatibus aut ullam fugiat earum, fuga delectus!',
-			link: '',
-			video: '',
-			podcastEmbed: '',
-			cta: 'CTA',
-			photo1: '',
-			photo2: '',
-			photo3: '',
-			photo4: '',
-			photo5: '',
-			photo6: ''
-		},
-		{
-			title: 'Podcast Editing & Production',
-			image: 'src/lib/assets/images/project1.jpg',
-			alt: '',
-			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium quod non, cumque repellat asperiores facilis porro unde voluptatibus maxime culpa exercitationem quo necessitatibus aut ullam fugiat earum, fuga delectus!',
-			link: '',
-			video: '',
-			podcastEmbed: '',
-			cta: 'CTA',
+			cta: 'Watch my Videos',
 			photo1: '',
 			photo2: '',
 			photo3: '',
@@ -55,14 +22,14 @@
 		},
 		{
 			title: 'Software Development & Web Development',
-			image: 'src/lib/assets/images/project1.jpg',
+			image: 'src/lib/assets/images/portfolio/Software.png',
 			alt: '',
 			description:
 				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium quod non, cumque repellat asperiores facilis porro unde voluptatibus maxime culpa exercitationem quo necessitatibus aut ullam fugiat earum, fuga delectus!',
-			link: '',
+			link: 'https://github.com/JobaAdewumi',
 			video: '',
 			podcastEmbed: '',
-			cta: 'CTA',
+			cta: 'Check my Github',
 			photo1: '',
 			photo2: '',
 			photo3: '',
@@ -70,6 +37,42 @@
 			photo5: '',
 			photo6: ''
 		},
+		{
+			title: 'Writing & Editing',
+			image: 'src/lib/assets/images/portfolio/writing.png',
+			alt: '',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium quod non, cumque repellat asperiores facilis porro unde voluptatibus maxime culpa exercitationem quo necessitatibus aut ullam fugiat earum, fuga delectus!',
+			link: 'https://jobaadewumi.substack.com',
+			video: '',
+			podcastEmbed: '',
+			cta: 'Read my Articles',
+			photo1: '',
+			photo2: '',
+			photo3: '',
+			photo4: '',
+			photo5: '',
+			photo6: ''
+		},
+		{
+			title: 'Podcast Editing & Production',
+			image: 'src/lib/assets/images/portfolio/Podcast.png',
+			alt: '',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium quod non, cumque repellat asperiores facilis porro unde voluptatibus maxime culpa exercitationem quo necessitatibus aut ullam fugiat earum, fuga delectus!',
+			link: 'https://creators.spotify.com/pod/show/joba-adewumi4',
+			video: '',
+			podcastEmbed:
+				'https://open.spotify.com/embed/show/4fIBS5rikdEvGh6tSJ93zW?utm_source=generator&t=0',
+			cta: 'Listen to my Podcast',
+			photo1: '',
+			photo2: '',
+			photo3: '',
+			photo4: '',
+			photo5: '',
+			photo6: ''
+		},
+
 		{
 			title: 'Photography & Photo Editing',
 			image: 'src/lib/assets/images/project1.jpg',
@@ -79,7 +82,7 @@
 			link: '',
 			video: '',
 			podcastEmbed: '',
-			cta: 'CTA',
+			cta: 'View my Pictures',
 			photo1: '',
 			photo2: '',
 			photo3: '',
@@ -92,26 +95,34 @@
 
 <Navbar />
 
-<section class="my-10">
+<section class="my-10 mt-20">
 	<h2 class="mx-3 my-3 bg-red-500 text-left font-pT text-3xl md:text-5xl xl:text-6xl">PORTFOLIO</h2>
 </section>
 
 <section>
 	<!-- Would have to do a for each statement to render the multiple data -->
 	{#each data as { title, image, alt, description, link, video, podcastEmbed, cta, photo1, photo2, photo3, photo4, photo5, photo6 }}
-		<div>
+		<div class="mt-5">
 			<h2 class="mx-3 my-3 bg-red-500 text-left font-pT text-xl capitalize md:text-3xl xl:text-4xl">
 				{title}
 			</h2>
 			<div class="mx-7 mb-3 mt-5 md:mx-12">
 				<div>
-					<img src={image} {alt} />
+					<img class="mx-auto rounded-3xl" src={image} {alt} />
 				</div>
 				<!-- Would have an if statement to check if video field is an empty string else show a video embed -->
 				{#if video !== ''}
-					<video src={video} controls>
+					<iframe
+						title="Life Podcast by Joba Adewumi"
+						class="mt-5 h-96 w-full rounded-xl"
+						src={video}
+						width="100%"
+						frameBorder="0"
+						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+					></iframe>
+					<!-- <video src={video} controls>
 						<track kind="captions" srclang="en" label="English" />
-					</video>
+					</video> -->
 				{/if}
 
 				{#if podcastEmbed !== ''}
@@ -131,12 +142,21 @@
 					<iframe
 						title="Life Podcast by Joba Adewumi"
 						class="mt-5 rounded-xl"
-						src="https://open.spotify.com/embed/show/4fIBS5rikdEvGh6tSJ93zW?utm_source=generator&t=0"
+						src={podcastEmbed}
 						width="100%"
 						height="232"
 						frameBorder="0"
 						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
 					></iframe>
+				{/if}
+				{#if title === 'Writing & Editing'}
+<!-- For writing -->
+					<iframe title="Life Podcast by Joba Adewumi"
+								src='https://jobaadewumi.substack.com/embed'
+								width='100%'
+								height='232'
+								class="mt-5 rounded-xl border-1 border-teal-500"
+								allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'></iframe>
 				{/if}
 
 				{#if description === ''}
@@ -149,16 +169,18 @@
 						<img src={photo6} alt="" />
 					</div>
 				{:else}
-					<p>
+					<p class="mt-3">
 						{description}
 					</p>
 				{/if}
-				<div class="flex justify-center">
-					<button class="h-12 w-auto rounded-2xl bg-blue-500 px-3 py-3" type="button">
-						<a href={link}>{cta}</a>
+				<div class="mt-3 flex justify-center">
+					<button class="h-auto w-auto rounded-2xl bg-blue-500 px-3 py-3" type="button">
+						<a target="_blank" rel="noopener noreferrer" href={link}>{cta}</a>
 					</button>
 				</div>
 			</div>
 		</div>
 	{/each}
 </section>
+
+<Footer />
