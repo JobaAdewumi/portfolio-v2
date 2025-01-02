3dbef55ae78d74c898a073d59d66e0c463db73fb
Joba Adewumi <jobaadewumis@gmail.com>
2025-01-03 00:30:17 +0100

    added footer structure

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,36 @@
+<script>
+	// Add any necessary script here
+</script>
+
+<footer class="bg-gray-800 flex flex-col items-center p-4 text-center text-white">
+    
+    <div class="max-w-12">
+		<a href="/">
+			<img src="src/lib/assets/images/multiple-logo-scattered.png" alt="Logo" />
+		</a>
+	</div>
+	<ul class="list-none p-0 mt-5">
+		<li class="mx-4 inline">
+			<a href="/about" class="text-white no-underline hover:underline">About</a>
+		</li>
+		<li class="mx-4 inline">
+			<a href="/portfolio" class="text-white no-underline hover:underline">Projects</a>
+		</li>
+		<li class="mx-4 inline">
+			<a href="/links" class="text-white no-underline hover:underline">Links</a>
+		</li>
+	</ul>
+	<div class="mb-4 mt-5">
+		<p class="text-sm">
+			<span class="mr-4"
+				>📧 <a href="mailto:jobaadewumis@gmail.com" class="text-white hover:underline"
+					>jobaadewumis@gmail.com</a  
+				></span
+			>
+			<span
+				>📱 <a href="tel:+2348022300886" class="text-white hover:underline">802 230 0886</a></span
+			>
+		</p>
+	</div>
+	<p>&copy; {new Date().getFullYear()} Joba Adewumi. All rights reserved.</p>
+</footer>
